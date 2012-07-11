b10de52a8cbf52ce00954d2b6dbb468a9970d5d5
Carlos Arrastia <carlos.arrastia@gmail.com>
2012-07-11 12:54:59 +0300

    Fixed widths for timelines and fixed zooming to keep centered where the mouse is

--- public/js/app.js
+++ public/js/app.js
@@ -23,6 +23,29 @@ $.template('photo', '\
     </li>');
 $.template('camera', '<li class="distribute-height cameramodel"><span>${camera}</span></li>');
         
+var TIME = {}
+TIME.second=1000;
+TIME.minute=TIME.second*60;
+TIME.hour=TIME.minute*60;
+TIME.day=TIME.hour*24;
+
+
+var TIME_PRECISION = [
+        10*TIME.second, //[0]
+        30*TIME.second, //[1]
+         1*TIME.minute, //[2]
+         3*TIME.minute, //[3]
+        10*TIME.minute, //[4]
+        30*TIME.minute, //[5]
+         1*TIME.hour, //[6]
+         3*TIME.hour, //[7]
+         9*TIME.hour, //[8]
+         1*TIME.day, //[9]
+         2*TIME.day, //[10]
+         4*TIME.day, //[11]
+         7*TIME.day //[12]
+];
+         
 
 /* create a singleton photosync object */
 var appManager = {
