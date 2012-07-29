1c0dd5dec02a50981677e94b6eef8589a6d66784
Carlos Arrastia <carlos.arrastia@gmail.com>
2012-07-29 18:49:30 +0300

    Improvements to save the thumbnails using exiftool

--- util/image_metadata_api.js
+++ util/image_metadata_api.js
@@ -1,7 +1,11 @@
 (function(){
     
     function getMetadata(filename, callback){
-        var ex = require('exiv2');
+        var ex = require('exiv2'),
+        im = require('imagemagick'),
+        fs = require('fs'),
+        spawn = require('child_process').spawn;
+
         
         var loadedData={
                 metadataLoaded:false,
@@ -32,14 +36,50 @@
             finishProcessing('metadata',error,metadata);
         });
         
-        ex.getImagePreviews(filename,function(err, previews) {
+        var callbackImagePreviews=null;
+        callbackImagePreviews=function(err, previews) {
             var data=previews;
             if (!err && data.length>0){
-                data=new Buffer(data[0].data).toString('base64');
+                data=data[0].data;
+                data=new Buffer(data).toString('base64');
+                finishProcessing('image',err,data);
+            }else{
+                //generate the thumbnail if it does not exist
+                var tempImage='/tmp/photosync-img-'+(new Date().getTime())+'.jpg';
+                im.resize({
+                    srcData: fs.readFileSync(filename, 'binary'),
+                    dstPath:tempImage,
+                    width:   160,
+                    height:   160,
+                    format:'jpg'
+                  }, function(err, stdout, stderr){
+                    if (err){
+                        finishProcessing('image',err,null);
+                        return;
+                    }
+                    
+                    //var fd =  fs.openSync("/tmp/manual-test"+(new Date().getTime())+'.jpg', 'w');
+                    //var buff = new Buffer(stdout, 'binary');
+                    
+                    /* in a child process save the metadata for the image */
+                    var exiftoolsave  = spawn('exiftool',
+                            ['-ThumbnailImage<='+tempImage,'-m', '-overwrite_original',filename]);
+                    
+                    exiftoolsave.on('exit', function (code, signal) {
+                        fs.unlink(tempImage);
+                        if (code == 0){
+                            console.log('UPDATED thumbnail for ' + filename);
+                            ex.getImagePreviews(filename,callbackImagePreviews);
+                        }else{
+                            console.log('FAILED UPDATING thumbnail for ' + filename);
+                            finishProcessing('image',err,null);
+                        }
+                    });
+                  });
             }
-            
-            finishProcessing('image',err,data);
-          });
+        };
+        
+        ex.getImagePreviews(filename,callbackImagePreviews);
     }
     function getMeaningfulData(metadata){
         var fields=["Exif.Image.DateTime","Exif.Image.Make","Exif.Image.Model","Exif.Photo.DateTimeDigitized","Exif.Photo.DateTimeOriginal"];
