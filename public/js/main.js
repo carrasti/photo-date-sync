c097784d29bcd53ed7b21d4df903ad15386de64f
Carlos Arrastia <carlos.arrastia@gmail.com>
2012-07-30 15:06:57 +0300

    Update public/js/main.js
    Removed not meaningful comments from copy-pasted code

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,3 @@
-// Author: Thomas Davis <thomasalwyndavis@gmail.com>
-// Filename: main.js
-
-// Require.js allows us to configure shortcut alias
-// Their usage will become more apparent futher along in the tutorial.
 require.config({
   paths: {
     'jquery': 'libs/jquery/jquery',
